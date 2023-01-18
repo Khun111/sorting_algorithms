8ded4b92f65d83c36e0e401ee782477c828cb053
Oluwatobi <amure387@gmail.com>
2023-01-18 13:55:15 +0100

    push

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,12 +1,20 @@
 #include "sort.h"
+/**
+ * selection_sort - sort an array using the relevant algo
+ * @array: array to be sorted
+ * @size: array size
+ */
 void selection_sort(int *array, size_t size)
 {
+/*Declare variables*/
 	size_t i, temp, j;
-
+/*Iterate through the entire array*/
 	for (i = 0; i < size; i++)
 	{
+/*Iterate from the next element to the rest of the array*/
 		for (j = i + 1; j < size; j++)
 		{
+/*If the next < current, swap*/
 			if (array[j] < array[i])
 			{
 				temp = array[j];
