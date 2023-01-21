78d70c41f6af19b6df778124dca2c39a87353803
Oluwatobi <amure387@gmail.com>
2023-01-21 19:53:50 +0100

    fixed selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -7,21 +7,27 @@
 void selection_sort(int *array, size_t size)
 {
 /*Declare variables*/
-	size_t i, temp, j;
+	size_t i, temp, j, min;
+
+	if (size < 2)
+		return;
 /*Iterate through the entire array*/
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
+		min = i;
 /*Iterate from the next element to the rest of the array*/
 		for (j = i + 1; j < size; j++)
 		{
 /*If the next < current, swap*/
-			if (array[j] < array[i])
-			{
-				temp = array[j];
-				array[j] = array[i];
-				array[i] = temp;
-			}
+			if (array[j] < array[min])
+				min = j;
 		}
-		print_array(array, size);
+		if (min != i)
+		{
+			temp = array[min];
+			array[min] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
 }
