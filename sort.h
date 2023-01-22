9124016399c09807e5139becdc51e90cfd4f73af
Oluwatobi <amure387@gmail.com>
2023-01-22 12:43:57 +0100

    push

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stdio.h>
 
 /**
@@ -12,9 +11,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
